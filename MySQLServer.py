381d59af589e9bb9cd36c8b985a7cc40b4d55107
Sunday Onoja <sundayonoja23@gmail.com>
2025-10-16 14:27:34 +0100

    python script that creates the database 6

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,11 +1,11 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
+    # Connect to MySQL Server
     connection = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='your_password'
+        password='your_password'  # Replace with your MySQL password
     )
 
     if connection.is_connected():
@@ -13,7 +13,7 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to the DB: {e}")
 
 finally:
